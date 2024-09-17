24fc535c4ab5324a2fd15f95c4bfda6439ee7f20
Karol Śpiewak <115652271+KarolS19@users.noreply.github.com>
2024-09-17 19:46:11 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from scraper.quotes_spider import QuotesSpider
-
-def main():
-    spider = QuotesSpider()
-    spider.scrape_quotes_and_authors()
-    spider.save_to_json()
-
-if __name__ == "__main__":
-    main()
